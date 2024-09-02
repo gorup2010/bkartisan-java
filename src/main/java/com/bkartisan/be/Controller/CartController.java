316a4bf2901af28c7a3353dcfd4a07b62ea6d81f
Hoi Lau <lauhoi2010@gmail.com>
2024-09-02 14:59:49 +0700

    Add products to cart, get cart and finish /login/success route

---
+++ src/main/java/com/bkartisan/be/Controller/CartController.java
@@ -0,0 +1,39 @@
+package com.bkartisan.be.Controller;
+
+import java.security.Principal;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bkartisan.be.Dto.CartItemDTO;
+import com.bkartisan.be.Service.CartService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/api/v1/carts")
+public class CartController {
+    
+    private CartService cartService;
+
+    @Autowired
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<Map<?, ?>> getCart(Principal principal) {
+        return ResponseEntity.ok(cartService.getCart(principal.getName()));
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> addItemToCart(@RequestBody CartItemDTO item, Principal principal) {
+        cartService.addProductToCart(principal.getName(), item.productId(), item.quantity());
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/bkartisan/be/Service/CartService.java
+++ src/main/java/com/bkartisan/be/Service/CartService.java
@@ -1,31 +1,79 @@
 package com.bkartisan.be.Service;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import com.bkartisan.be.Entity.Product;
 
 @Service
 public class CartService {
-    private RedisOperations<String, Map<String, Integer>> redisOperations;
+
+    private String ID_PREFIX = "cart:";
+
+    private StringRedisTemplate redisOperations;
+    private ProductService productService;
 
     @Autowired
-    public CartService(RedisOperations<String, Map<String, Integer>> redisOperations) {
+    public CartService(StringRedisTemplate redisOperations, ProductService productService) {
+        this.productService = productService;
         this.redisOperations = redisOperations;
     }
 
-    public Map<?,?> getCart(String username) {
-        return redisOperations.opsForHash().entries(username);
+    public Map<String, Integer> getCart(String username) {
+        String key = ID_PREFIX + username;
+        Map<String, Integer> cart = redisOperations.opsForHash().entries(key).entrySet().stream()
+                .collect(Collectors.toMap(
+                        entry -> entry.getKey().toString(),
+                        entry -> Integer.valueOf(entry.getValue().toString())));
+        return cart;
     }
 
     public Integer getTotalPrice(String username) {
-        String entry = "cart:" + username;
-        return redisOperations.opsForHash().values(username).stream().mapToInt(Integer::intValue).sum();
+        String key = ID_PREFIX + username;
+        // No items in cart - return 0
+        if (!redisOperations.hasKey(key)) {
+            return 0;
+        }
+
+        try {
+            Map<?, ?> entries = redisOperations.opsForHash().entries(key);
+
+            // First, map to a stream of price of each item. Then sum it up with
+            // Collectors.summingInt.
+            Integer totalPrice = entries.entrySet().stream().map(e -> {
+                Product prod = productService.getProduct(Integer.parseInt(e.getKey().toString()));
+                Integer price = prod.getPrice();
+                return price * (Integer) Integer.parseInt(e.getValue().toString());
+            }).collect(Collectors.summingInt(Integer::intValue));
+
+            return totalPrice;
+        } catch (Exception e) {
+            System.out.println("Error in getTotalPrice: " + e.getMessage());
+            throw e;
+        }
     }
 
     public Integer getTotalItems(String username) {
-        String entry = "cart:" + username;
-        return redisOperations.opsForHash().entries(entry).size();
+        String key = ID_PREFIX + username;
+        return redisOperations.opsForHash().entries(key).size();
+    }
+
+    // public List<Map<String, Integer>> getCartItems(String username) {
+    // return
+    // redisOperations.opsForHash().values(username).stream().map(CartItem::new).collect(Collectors.toList());
+    // }
+
+    public void addProductToCart(String username, Integer productID, Integer quantity) {
+        String key = ID_PREFIX + username;
+        if (redisOperations.opsForHash().hasKey(key, productID.toString())) {
+            redisOperations.opsForHash().increment(key, productID.toString(), quantity);
+        } else {
+            redisOperations.opsForHash().put(key, productID.toString(),
+                    quantity.toString());
+        }
     }
 }
